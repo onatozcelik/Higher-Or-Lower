f0db1845f3fc7af09c23cd026fa7c16329e42720
onatozcelik <58370802+onatozcelik@users.noreply.github.com>
2024-01-21 21:25:14 +0300

    added game functionality

--- main.py
+++ main.py
@@ -23,8 +23,41 @@ def print_versus(account_1,account_2,versus=VERSUS_ART):
     print(f"Against B: {account_format(account_2)}")
     return input("Which has more followers A or B ? \n").upper()
 
+def check_guess(guess,account1_followers,account2_followers):
+    if account1_followers> account2_followers:
+        return guess=='A'
+    elif account2_followers > account1_followers:
+        return guess== 'B'
 
-account1=select_account()
-account2=select_account()
 
-print(print_versus(account1,account2))
+def game():
+    print(logo)
+    game_continue = True
+    score=0
+    account1=select_account()
+    account2=select_account()
+    while game_continue:
+        account1=account2
+        account2=select_account()
+        while account1 == account2:
+            account2=select_account()
+        guess=print_versus(account1,account2)
+        account1_followers = int(account1["follower_count"])
+        account2_followers = int(account2["follower_count"])
+        guess_correct= check_guess(guess,account1_followers,account2_followers)
+        if guess_correct:
+            score +=1
+            print(f"Correct ! Current Score: {score}")
+        else:
+            game_continue=False
+            print(f"You Lose ! Score : {score} ")
+            if input("Do You Want To Play Aagain Y or N ?").upper() =="Y":
+                game()
+            else:
+                print("Goodbye !")
+
+    
+
+
+
+game()
